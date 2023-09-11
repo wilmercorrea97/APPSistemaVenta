d03263254a9d26e70fd95ad9834bb75354cfecec
Invertec <112434597+Invertec@users.noreply.github.com>
2023-09-11 13:49:33 -0300

    Configurando pagina y modal Producto

--- src/app/Components/layout/Pages/venta/venta.component.ts
+++ src/app/Components/layout/Pages/venta/venta.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+import { MatTableDataSource } from '@angular/material/table';
+
+import { ProductoService } from 'src/app/Services/producto.service';
+import { VentaService } from 'src/app/Services/venta.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
+
+import { Producto } from 'src/app/Interfaces/producto';
+import { Venta } from 'src/app/Interfaces/venta';
+import { DetalleVenta } from 'src/app/Interfaces/detalle-venta';
+
+import Swal from 'sweetalert2';
+
+
+
+
 @Component({
   selector: 'app-venta',
   templateUrl: './venta.component.html',
